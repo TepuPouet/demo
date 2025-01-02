07eea5c7a37db82e81df85fd2b6432bd5ff7f628
pouetpouet <pouet9452@gmail.com>
2025-01-02 13:35:23 +0100

    Read and create user ok

--- angularclient/src/app/app.component.ts
+++ angularclient/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports : [RouterModule]
 })
 export class AppComponent {
-  title = 'angularclient';
+
+  title: string;
+
+  constructor() {
+    this.title = 'Spring Boot - Angular Application';
+  }
 }
---
+++ angularclient/src/app/user-form/user-form.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../service/user-service.service';
+import { User } from '../model/user';
+import { NgForm, FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css'],
+  imports: [FormsModule],
+})
+export class UserFormComponent {
+
+  user: User;
+
+  constructor(
+    private route: ActivatedRoute,
+      private router: Router,
+        private userService: UserService) {
+    this.user = new User();
+  }
+
+  onSubmit() {
+    this.userService.save(this.user).subscribe({
+      next: value => this.gotoUserList(),
+      error: err => this.router.navigate(['/']),
+      });
+  }
+
+  gotoUserList() {
+    this.router.navigate(['/users']);
+  }
+}
